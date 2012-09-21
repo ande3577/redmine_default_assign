dac338d8f694d354f29370ceaf5610651ec70375
David S Anderson <ande3577@gmail.com>
2012-09-21 00:08:01 -0500

    Removed debug message

--- lib/default_assign/hooks/default_assign_issues_hooks.rb
+++ lib/default_assign/hooks/default_assign_issues_hooks.rb
@@ -1,7 +1,6 @@
 class DefaultAssignIssueHook < Redmine::Hook::ViewListener
   def controller_issues_new_before_save(context={})
     if context[:issue].assigned_to.nil? && (context[:issue].category.nil? || context[:issue].category.assigned_to.nil?)
-        config.logger.debug "dsanderson: Category: " + context[:issue].category.to_s
         selected = context[:project].default_assignee.id unless context[:project].default_assignee.blank?
         selected ||= Setting.plugin_redmine_default_assign['default_assignee_id'] unless Setting.plugin_redmine_default_assign['default_assignee_id'].blank? or 
   											not context[:project].assignable_users.member?(User.find(Setting.plugin_redmine_default_assign['default_assignee_id']).id)
